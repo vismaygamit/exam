ce74d1fe7764517fd7b93fc1b2e85afdd5f2fd8d
exam613 <SRIMCA\exam613@cs-u613.srimca.local>
2018-11-23 10:17:05 +0530

    Commitusdetail

--- ContactUs.php
+++ ContactUs.php
@@ -1,4 +1,4 @@
 DETAILS:This is the details of project  leader.
 <?php echo 'email:xyz@gmail.com add:Vertict AREA Phoneno:35564646' ?>
-DETAILS:This is the details of project  leader.   <?php echo 'email:xyz@gmail.com add:Vertict AREA Phoneno:35564646' ?>DETAILS:This is the details of project  leader.   
-<?php echo 'email:xyz@gmail.com add:Vertict AREA Phoneno:35564646' ?>
+
+this is updation from details
